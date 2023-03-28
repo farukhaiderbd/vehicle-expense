6cdd4797980c6c20c908c96654f4798f798b510f
faruk Haider <farukhaidar3@gmail.com>
2023-03-28 13:44:01 +0600

    migration update

--- database/migrations/2023_03_27_000017_add_relationship_fields_to_vehicle_documents_table.php
+++ database/migrations/2023_03_27_000017_add_relationship_fields_to_vehicle_documents_table.php
@@ -13,6 +13,10 @@ class AddRelationshipFieldsToVehicleDocumentsTable extends Migration
             $table->foreign('company_vehicle_id', 'company_vehicle_fk_8245709')->references('id')->on('company_vehicles');
             $table->unsignedBigInteger('created_by_id')->nullable();
             $table->foreign('created_by_id', 'created_by_fk_8245716')->references('id')->on('users');
+            $table->unsignedBigInteger('company_id')->nullable();
+            $table->foreign('company_id', 'company_fk_8246774')->references('id')->on('companies');
+            $table->unsignedBigInteger('vehicle_document_type_id')->nullable();
+            $table->foreign('vehicle_document_type_id', 'vehicle_document_type_fk_8246484')->references('id')->on('vehicle_document_types');
         });
     }
 }
